90abbfd3bddb3068d3e1dc1c1b3f8dc767ef11c0
BasilAssi <tarifibasel0@gmail.com>
2022-07-02 21:50:24 +0300

    version 1.1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends  AppCompatActivity {
+  //developer .android .com
+    private EditText edtName;
+    private EditText edtpass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { // callback  , method life cycle call back
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //R connect btw java and xml
+        edtName=findViewById(R.id.edtName);
+        edtpass=findViewById(R.id.editTextTextPassword);
+    }
+   public void btnSumbitOnClick(View view){
+    String name = edtName.getText().toString();
+    String password = edtpass.getText().toString();
+    if(name.equals("basil") && password.equals("basil"))
+    Toast.makeText(this ,"Welcome",Toast.LENGTH_LONG).show();
+
+
+        Toast.makeText(this ,"Ops !! , Wrong Login",Toast.LENGTH_LONG).show();
+   }
+
+
+}
